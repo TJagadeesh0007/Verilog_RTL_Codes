f323cb1e878600a23ef42da735a076900b54e617
TJagadeesh0007 <jagadeeshtaralada@gmail.com>
2026-02-13 23:31:02 +0530

    Add wire w3 for carry in full adder module

--- Adders_subs/Full_using_half_addr/full_using_half.v
+++ Adders_subs/Full_using_half_addr/full_using_half.v
@@ -1,6 +1,6 @@
 module full_using_half(input a,b,c, output sum,carry);
 	
-	wire w1,w2;
+	wire w1,w2,w3;
 
 	half_addr g1 (.a(a), .b(b), .sum(w1), .carry(w2));
 	half_addr g2 (.a(w1), .b(c), .sum(sum), .carry(w3));
